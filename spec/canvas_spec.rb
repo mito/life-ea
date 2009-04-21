bb2005f3227cffcfea6d9d96c8059f559deeb6e4
mito <mito@tennismanager.sk>
2009-04-21 16:32:02 +0200

    some work

--- spec/canvas_spec.rb
+++ spec/canvas_spec.rb
@@ -10,4 +10,30 @@ describe "Canvas" do
         canvas = Canvas.new '111111111111111111111111111111111111'
         canvas.genom.should == '111111111111111111111111111111111111' 
     end
+
+
+    it "should generate next generation canvas" do 
+        pending 'need to fix cell switching first'
+        canvas = Canvas.new '100000000000000000000000000000000000'
+        canvas2 = canvas.live 
+
+        canvas2.genom.should = '000000000000000000000000000000000000'
+    end
+
+    it 'should return number of neighbours of corner cell' do
+        pending 'need to get cell on exect location'
+        canvas = Canvas.new '010000-000000-000000-000000-000000-000000'
+        canvas.neighbours(0,0).should = 1
+
+    end
+
+    it 'should return cell value' do
+        canvas = Canvas.new '010000-000000-000000-000000-000000-000000'
+        canvas.cell(0,0).should == 0
+    end
+
+    it 'should return cell value1' do
+        canvas = Canvas.new '010000-000000-000000-000000-000000-000000'
+        canvas.cell(0,1).should == 1
+    end
 end
--- src/canvas.rb
+++ src/canvas.rb
@@ -6,4 +6,17 @@ class Canvas
     def genom 
         @genom
     end
+
+
+    def live 
+    end
+
+    def neighbours row, column
+     
+    end
+
+    def cell row, column
+        index = row * 6 + column
+        @genom[index].chr.to_i
+    end
 end
